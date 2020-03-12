2c6329372a09e3652b93b428e40c048cc884f448
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2020-03-12 01:11:01 +0000

    Many changes made to create spreadsheet for College

--- ClickerRegister.py
+++ ClickerRegister.py
@@ -5,6 +5,8 @@ import openpyxl
 import re
 import copy
 import os
+import csv
+import shutil
 
 """
 Read in TurningPoint file and return list of clicker ID's
@@ -70,7 +72,7 @@ def getStartHour(fName):
 
 """
  getActivityRoot create end of string for activity
- input turningPointFilename
+ input tpFn
  output string of form dd.<Mon>.yyyy_hh00
 """         
 
@@ -118,16 +120,15 @@ def buildStudentDict(filename):
     studentData = {}
     for row in ws.iter_rows(max_col=7,values_only=True):
         key = row[2]
-        if row[0] is not None:
+        if len(row) > 0:
             if not key in studentData.keys() :
                 studentData[key] = {}
                 studentData[key]['First'] = row[4]
                 studentData[key]['Surname'] = row[6]
                 studentData[key]['clicker'] = row[3]
-                studentData[key]['courses'] = [row[0] + str(row[1]),]
+                studentData[key]['courses'] = [str(row[0]) + str(row[1]),]
             else:
-                studentData[key]['courses'].append(row[0] + str(row[1]))     
-      
+                studentData[key]['courses'].append(str(row[0]) + str(row[1]))     
             
     return(studentData)
 
@@ -152,29 +153,41 @@ Return subset of studentData with a list of specific clicker ID's
 
 input: studentData (dict), IDs (list)
 output: list with keys of studentData that has clicker ID's matching the list of IDs
+if there are any malformed clicker IDs, add the student ID to 
+a separate list of student IDs 
+
 """
 
 def selectIDStudentData(data,IDs):
    
     studentIDs = []
-    for k,v in data.items():
-        if v['clicker'] in IDs:
-            studentIDs.append(k)
-            
-    return(studentIDs)     
-        
-        
+    malformedIDs = []
+    for k in data.keys(): 
+        if data[k] and type(data[k]) == dict:
+            v = data[k]
+            if v['clicker'] and type(v['clicker']) == str:
+                if ( len(v['clicker']) == 6 ):
+                    if  v['clicker'] in IDs:
+                        studentIDs.append(k)
+                else:
+                    malformedIDs.append(k)
+            else:
+                malformedIDs.append(k)
+        else:
+            malformedIDs.append(k) 
+                    
+    return(studentIDs,malformedIDs)     
     
 """
 Create a dict with studentData based on two lists; 1 list being students registered on a course; another
 being students who attended the lecture
 New dict has two news values for each key 
-1 key is Present (Yes for present, no for absent)
+1 key is Present (True for present or has a malformed ID, False for absent)
 1 key registered on course
 
 """    
 
-def collateStudentsInLecture(allStudentData,registered,attended):
+def collateStudentsInLecture(allStudentData,registered,attended,malformed):
     r = set(registered)
     a = set(attended)
        
@@ -183,7 +196,7 @@ def collateStudentsInLecture(allStudentData,registered,attended):
     
     for i in rUa:
         thisStudentData[i] = copy.deepcopy(allStudentData[i])
-        thisStudentData[i]['Present'] = i in attended
+        thisStudentData[i]['Present'] = i in attended or i in malformed
         thisStudentData[i]['Registered'] = i in registered
     
     
@@ -198,8 +211,8 @@ Create a spreadsheet with present/absent data
 
 def createAttendanceSpreadsheet(collated,date,course,activity,CRN,folder):
     wb = openpyxl.Workbook()
-    ws = wb.active()
-#    ws = wb.create_sheet("Attendance")
+#    ws = wb.active()
+    ws = wb.create_sheet("Attendance")
     names=("Activity",activity,"CRN",CRN)
     ws.append(names)
     names=["First name","Surname","Student ID","Clicker ID","Present",]
@@ -227,45 +240,104 @@ def createAttendanceSpreadsheet(collated,date,course,activity,CRN,folder):
             
     wb.save(Path(folder) / fname)
     
+"""
+Update a spreadsheet of the register report with present/absent data
+
+"""
+
+def updateRegisterReport(collated,activity,activityFn):
+# Create new worksheet to store data
+    tFn = "/Users/upac004/Downloads/tmp.csv"
+    with open(tFn,'w',newline='') as csvOut:
+        wsO = csv.writer(csvOut,delimiter=',',quotechar='"')
+    
+# first worksheet has the useful data
+        with open(activityFn, newline='') as csvIn:
+            wsI = csv.reader(csvIn,delimiter=',',quotechar='"')
+            for row in wsI:
+                a = row[0]
+                if ( a == activity ):
+                    thisID = row[4]
+                    p = findAttendance(thisID,collated)
+                    row[8] = p
+                wsO.writerow(row)
+
+# overwrite original file with temp file
+    shutil.move(tFn, activityFn)
+  
+
+"""
+return information about whether a particular student for a session was present/absent
+
+input : student IDs (string), collated Dict
+output : "Y" if student present, "N" if absent, "Not found" if id is not in dict
+"""
+
+def findAttendance(IDs,collated):
+    
+    ID = int(IDs)
+    if ID in collated:
+        v = collated[ID]
+        if v["Present"]:
+            return("Y")
+        else:
+            return("N")
+    else:
+        return("Not found")
         
+    
 """
 From the name of the TunrningPoint file and course
 identify the acivitity and CRN from the relevant file
 
-input : clickerFN, course, activityFN (spreadsheet)
+input : clickerFN, course, activityFN (csv file)
 output: activity (string), CRN (string)
 """
-def getActivityCRN(clickerFN,course,activityFN):
-    AR = getActivityRoot(clickerFN)
+def getActivityCRN(clickerFn,course,activityFn):
+    AR = getActivityRoot(clickerFn)
 
-# first worksheet has the useful data
-    activityFile = openpyxl.load_workbook(activityFN,data_only=True)    
-    ws = activityFile.worksheets[0]
-    
-    for row in ws.iter_rows(max_col=5,values_only=True):
-        c = row[1]
-        if ( c == course ):
-            if row[0].find(AR) > -1:
-                return((row[0], row[3]))
+    with open(activityFn, newline='') as csvfile:
+        ws = csv.reader(csvfile,delimiter=',',quotechar='"')
+        for row in ws:
+            c = row[1]
+            if ( c == course ):
+                if row[0].find(AR) > -1:
+                    return((row[0], row[3])) 
+        return(None,None)        
 
    
 """
-Put everything together
+Update the register report for a single clicker file
 
 """    
-def runThis(clickerFn,course, outputFolder, studentFn,activityFN):
-    (activity,CRN) = getActivityCRN(clickerFN,course,activityFN)
-    clickerIDs = ClickerRegister(clickerFn)
-    studentData = buildStudentDict(studentFn)
-    studentsOnThisCourse = selectCourseStudentData(studentData,course)
-    studentIDsAttending = selectIDStudentData(studentData,clickerIDs)
-    studentsAttending = collateStudentsInLecture(studentData,studentsOnThisCourse,studentIDsAttending)
-    date = getDate(clickerFn)
-    createAttendanceSpreadsheet(studentsAttending,date,course,activity,CRN,outputFolder)
+def updateRegisterReportForAClickerFile(clickerFn,course, outputFolder, studentFn,activityFn):
+    (activity,CRN) = getActivityCRN(clickerFn,course,activityFn)
+#  Don't do anything if you cannot find the right activity date/time
+    if activity:
+        clickerIDs = ClickerRegister(clickerFn)
+        studentData = buildStudentDict(studentFn)
+        studentIDsAttending,malformedIDs = selectIDStudentData(studentData,clickerIDs)
+        studentsAttending = collateStudentsInLecture(studentData,studentData.keys(),studentIDsAttending,malformedIDs)
+        updateRegisterReport(studentsAttending,activity,activityFn)
+   
     
+"""
+Pick out all .tpzx files in a directory and update register report for all of them 
+"""
     
+def updateReportForAFolder(clickerDir,course, outputFolder="/Users/upac004/Downloads/", studentFn="/Users/upac004/Downloads/Copy of Clicker Data CT Fixed (2).xlsx",activityFn="/Users/upac004/Downloads/AttendanceRegisterReport_Term2.csv"):
     
-    
-    
-    
+    with os.scandir(clickerDir) as it:
+        for entry in it:
+            if re.search(r'.tpzx',entry.name):
+                print("Processing " + entry.name)
+                updateRegisterReportForAClickerFile(entry.path,course, outputFolder, studentFn,activityFn)
+
+                
+                
+sFn = "/Users/upac004/Downloads/Copy of Clicker Data CT Fixed (2).xlsx"
+aFn = "/Users/upac004/Downloads/AttendanceRegisterReport_Term2.csv"
+                
+            
+
     
